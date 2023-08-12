56599c47cd05303be41aedd58e6e22052b6c9938
MattBuchs <mattbuchs25@gmail.com>
2023-08-13 00:37:11 +0200

    add timer on the first window

--- secondWindow.js
+++ secondWindow.js
@@ -1,5 +1,11 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer } = require("electron");
 
-ipcRenderer.on('button-clicked', (event, message) => {
-  document.getElementById('messageFromMainWindow').textContent = `Message reçu : ${message}`;
+ipcRenderer.on("button-clicked", (event, message) => {
+    document.getElementById(
+        "messageFromMainWindow"
+    ).textContent = `Message reçu : ${message}`;
+});
+
+ipcRenderer.on("play-timer", (event, message) => {
+    document.querySelector(".container p").textContent = message;
 });
