e0157368358596f71f174e27b2724c00a3515eba
Matt Buchs <110405782+MattBuchs@users.noreply.github.com>
2024-07-14 17:16:02 +0200

    2 buttons added + bug/ui fix

--- src/core/mainWindow/UI/messages.js
+++ src/core/mainWindow/UI/messages.js
@@ -4,13 +4,19 @@ const formMessage = document.querySelector("#form-message");
 const message = document.querySelector("#message");
 const phrasesSelected = document.querySelector("#phrases-select");
 const btnClear = document.querySelector("#btn-clear");
+const supprMessage = document.querySelector("#img-delete");
+const getMessage = document.querySelector("#img-back");
 
 const messagesObj = {
+    message: null,
+
     init() {
         formMessage.addEventListener("submit", this.sendMessage.bind(this));
         btnClear.addEventListener("click", this.clearMessage.bind(this));
         phrasesSelected.addEventListener("change", this.changeValue.bind(this));
         message.addEventListener("input", this.updateSelect.bind(this));
+        supprMessage.addEventListener("click", this.deleteMessage.bind(this));
+        getMessage.addEventListener("click", this.getMessage.bind(this));
     },
 
     sendMessage(e) {
@@ -19,9 +25,9 @@ const messagesObj = {
         if (message.value !== "") {
             phrasesSelected.selectedIndex = 0;
             const messageWrited = message.value;
-            message.value = "";
 
             ipcRenderer.send("send-message", messageWrited);
+            this.message = messageWrited;
         }
     },
 
@@ -39,8 +45,17 @@ const messagesObj = {
         phrasesSelected.selectedIndex = 0;
     },
 
+    getMessage() {
+        console.log(message, this.message);
+        message.value = this.message;
+    },
+
     clearMessage() {
         ipcRenderer.send("clear-message");
+    },
+
+    deleteMessage() {
+        message.value = "";
     },
 };
 
--- src/core/secondWindow/messages.js
+++ src/core/secondWindow/messages.js
@@ -4,10 +4,8 @@ const messageReceived = document.querySelector("#message-received");
 
 ipcRenderer.on("send-message", (_, message) => {
     messageReceived.textContent = message;
-    messageReceived.classList.remove("not-indice");
 });
 
 ipcRenderer.on("clear-message", () => {
-    messageReceived.textContent = "indice à venir...";
-    messageReceived.classList.add("not-indice");
+    messageReceived.textContent = "";
 });
